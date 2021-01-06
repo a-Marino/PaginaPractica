6c8c0d92964c34d5a5d1d53a1d80793a671d8ac0
a-Marino <andresmarinolaiolo@hotmail.com.ar>
2021-01-05 21:04:49 -0300

    nav mejorado y se agrego la pagina de mi universidad

---
+++ js/universidad.js
@@ -0,0 +1,4 @@
+var imgPaginaUPSO = document.querySelector('#pagina-upso');
+imgPaginaUPSO.addEventListener('click', function () {
+    window.open('https://www.upso.edu.ar/', '_blank');
+});
---
+++ js/universidad.ts
@@ -0,0 +1,5 @@
+const imgPaginaUPSO = document.querySelector<HTMLImageElement>('#pagina-upso');
+
+imgPaginaUPSO.addEventListener('click', ()=>{
+	window.open('https://www.upso.edu.ar/', '_blank');
+});
